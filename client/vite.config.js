21c3eb12b58e2fd7a23d170484c4506b51dc426e
Simer <simerseehra13@gmail.com>
2025-07-04 21:00:32 +0530

    vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
